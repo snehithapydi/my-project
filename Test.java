14ac7ee3f847c99a63691114521f572c04ad04c7
snehithapydi <32194129+snehithapydi@users.noreply.github.com>
2017-10-06 16:37:14 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+	public static void main(String[] args) {
+		Object o = getObject( );
+		Object o2=o;
+		System.out.println(o2.toString());
+
+	}
+	private static Object getObject(){
+		return null;
+	}
+
+}
